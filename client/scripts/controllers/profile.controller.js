c181eecbca4f12f6dc0962448665e26181be50a4
Karan Sangha <iamkaransangha@gmail.com>
2016-12-31 18:20:40 -0800

    add profile picture support
    user can now add a profile picture! :D

--- client/scripts/controllers/profile.controller.js
+++ client/scripts/controllers/profile.controller.js
@@ -1,33 +1,59 @@
-import { _ } from 'meteor/underscore';
-import { Controller } from 'angular-ecmascript/module-helpers';
+import {
+    _
+} from 'meteor/underscore';
+import {
+    MeteorCameraUI
+} from 'meteor/okland:camera-ui';
+import {
+    Controller
+} from 'angular-ecmascript/module-helpers';
 
 export default class ProfileCtrl extends Controller {
-  constructor() {
-    super(...arguments);
+    constructor() {
+        super(...arguments);
 
-    const profile = this.currentUser && this.currentUser.profile;
-    this.name = profile ? profile.name : '';
-  }
+        const profile = this.currentUser && this.currentUser.profile;
+        this.name = profile ? profile.name : '';
+    }
 
-  updateName() {
-    if (_.isEmpty(this.name)) return;
+    updatePicture() {
+        MeteorCameraUI.getPicture({
+            width: 60,
+            height: 60
+        }, (err, data) => {
+            if (err) return this.handleError(err);
 
-    this.callMethod('updateName', this.name, (err) => {
-      if (err) return this.handleError(err);
-      this.$state.go('tab.chats');
-    });
-  }
+            this.$ionicLoading.show({
+                template: 'Updating picture...'
+            });
 
-  handleError(err) {
-    this.$log.error('Profile save error ', err);
+            this.callMethod('updatePicture', data, (err) => {
+                this.$ionicLoading.hide();
+                this.handleError(err);
+            });
+        });
+    }
 
-    this.$ionicPopup.alert({
-      title: err.reason || 'Save failed',
-      template: 'Please try again',
-      okType: 'button-positive button-clear'
-    });
-  }
+    updateName() {
+        if (_.isEmpty(this.name)) return;
+
+        this.callMethod('updateName', this.name, (err) => {
+            if (err) return this.handleError(err);
+            this.$state.go('tab.chats');
+        });
+    }
+
+    handleError(err) {
+        if (err.error == 'cancel') return;
+        this.$log.error('Profile save error ', err);
+
+        this.$ionicPopup.alert({
+            title: err.reason || 'Save failed',
+            template: 'Please try again',
+            okType: 'button-positive button-clear'
+        });
+    }
 }
 
 ProfileCtrl.$name = 'ProfileCtrl';
-ProfileCtrl.$inject = ['$state', '$ionicPopup', '$log'];
+ProfileCtrl.$inject = ['$state', '$ionicLoading', '$ionicPopup', '$log'];
